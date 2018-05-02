e1621d1a859b76e9eb5d861958d69a8a7c573f78
Shridhar Sharma <shridharkaushik29@gmail.com>
2018-05-02 14:57:58 +0530

    Update bower.php

--- config/bower.php
+++ config/bower.php
@@ -1,5 +1,13 @@
 <?php
 
 return [
-    "url" => env("APP_URL") . "/bower_components"
+    "url" => env("APP_URL") . "/bower_components",
+    "components_info" => [
+        "ace-builds" => [
+            "main" => [
+                "src-min/ace.js",
+                "src-min/ext-language_tools.js",
+            ]
+        ]
+    ]
 ];
